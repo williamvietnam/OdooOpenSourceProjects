0e7454c1cf3f96e5699c885a22eac8eb0c821cc6
NguyenBangGiang <williamnb.vn@gmail.com>
2023-01-25 22:03:20 +0700

    nbgsoftware document manifest

---
+++ OdooOpenSourceModules/nbgsoftware_document/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'NBGSoftware Document',
+    'version': '14.0.1.0',
+    'category': 'NBGSoftware',
+    'summary': 'NBGSoftware Document',
+    'description': """
+        * NBGSoftware Document
+    """,
+    'website': '',
+    'author': '',
+    'depends': ['nbgsoftware_application'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/assets.xml',
+        'security/document_security.xml',
+        'security/app_security.xml',
+        'views/document_document.xml',
+        'views/document_document_public.xml',
+        'views/document_workspace.xml',
+        'views/menu.xml',
+        'views/portal_document_templates.xml',
+        'views/ui_app_document_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'qweb': [
+        'static/src/xml/view_home_app.xml',
+    ],
+}
