af22afd8e6157d2155a549b02698f728b734c207
GiangNB2 <giangnb2@rikkeisoft.com>
2023-02-01 15:13:05 +0700

    nbgsoftware_notification: fix display notification badge for portal user

---
+++ OdooOpenSourceModules/nbgsoftware_notification/static/src/js/portal_notification_badge.js
@@ -0,0 +1,22 @@
+odoo.define('nbgsoftware_notification.PortalNotificationButton', function (require) {
+"use strict";
+
+var rpc = require("web.rpc");
+
+(async () => {
+    const categories = await rpc.query({
+      model: "notification.notification.public",
+      method: "get_notification_count_unread",
+      args:[],
+     }).then(function(data){
+       let notificationBadge = $(`<li class="nav-item">
+                  <a class="o_notification_badge" style="cursor: pointer" aria-label="Notification Badge" accesskey="h" href="/notifications">
+                      <i role="img" aria-label="Notifications" class="fa fa-bell" style="font-size: 22px"/>
+                      <span class="o_NotificationBadge_counter badge badge-pill"
+                           style="background-color: red; font-size: 9px; position: relative; color: white; transform: translate(-16px, -12px);">${data}<span>
+                  </a>
+               </li>`);
+       $('#top_menu').before(notificationBadge);
+     });
+})();
+});
