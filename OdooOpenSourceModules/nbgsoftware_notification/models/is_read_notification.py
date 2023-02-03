be94fbf288068e3d358b8039c950812424aefd2a
NguyenBangGiang <williamnb.vn@gmail.com>
2023-02-03 20:56:26 +0700

    nbgsoftware_notification: convert execute query sql of is_read_notification table to ORM model with models folder

---
+++ OdooOpenSourceModules/nbgsoftware_notification/models/is_read_notification.py
@@ -0,0 +1,113 @@
+from odoo import models, fields, api, _
+
+
+class IsReadNotification(models.Model):
+    _name = "is.read.notification"
+    _description = "Is Read Notification"
+
+    user_id = fields.Char(string="user_id")
+    notification_id = fields.Char(string="notification_id")
+
+    # get value count unread to show on notification badge for account of internal user and portal user
+    @api.model
+    def get_notification_count_unread(self):
+        user_id = self.env.user.id
+        count_notification = self.env['notification.notification.public'].search_count(
+            [('user_ids.id', '=', user_id)])
+        count_notification_read = self.env['is.read.notification'].search_count([('user_id', '=', user_id)])
+        count = count_notification - count_notification_read
+        if count < 0:
+            count = 0
+        return count
+
+    # list all notifications include notifications read & unread
+    def list_of_notifications(self):
+        user_id = self.env.user.id
+        notifications_list = self.env['notification.notification.public'].search([('user_ids.id', '=', user_id)])
+        return notifications_list
+        # list all notifications read
+
+    # list notifications list read
+    def list_of_read_notifications(self):
+        user_id = self.env.user.id
+        notifications_list_read = self.env['is.read.notification'].search([('user_ids.id', '=', user_id)])
+        return notifications_list_read
+
+    @api.model
+    def is_read_notification_through_notification_id(self, notification_id):
+        user_id = self.env.user.id
+        notifications_list_read = self.env['is.read.notification'].sudo().search(
+            ['&', ('notification_id', '=', notification_id), ('user_id', '=', user_id)])
+        if notifications_list_read:
+            return True
+        else:
+            return False
+
+    @api.model
+    def update_list_read_notification(self, notification_list):
+        for i in notification_list:
+            self.update_is_read_notification(i)
+
+    @api.model
+    def delete_list_unread_notification(self, notification_list):
+        for i in notification_list:
+            self.delete_is_read_notification(i)
+
+    @api.model
+    def update_is_read_notification(self, notification_id):
+        user_id = self.env.user.id
+        if notification_id and not self.check_is_read(notification_id):
+            self.env['is.read.notification'].create({'user_id': user_id, 'notification_id': notification_id})
+        else:
+            return False
+
+    # check notification read or unread with notification_id and user_id conditions
+    @api.model
+    def check_is_read(self, notification_id):
+        user_id = self.env.user.id
+        notifications_list_read = self.env['is.read.notification'].sudo().search(
+            ['&', ('notification_id', '=', notification_id), ('user_id', '=', user_id)])
+        if notifications_list_read:
+            return True
+        else:
+            return False
+
+    # check notification read or unread with notification_id condition
+    @api.model
+    def check_is_read_notification(self, notification_id):
+        notifications_list_read = self.env['is.read.notification'].sudo().search(
+            [('notification_id', '=', notification_id)])
+        if notifications_list_read:
+            return True
+        else:
+            return False
+
+    # remove notification_id from notifications list read with notification_id and user_id conditions
+    @api.model
+    def delete_is_read_notification(self, notification_id):
+        user_id = self.env.user.id
+        if self.check_is_read(notification_id):
+            self.env["is.read.notification"].search(
+                ['&', ('notification_id', '=', notification_id), ('user_id', '=', user_id)]).unlink()
+        else:
+            return False
+
+    # get all notifications include notifications read and notifications unread
+    @api.model
+    def get_notifications_data(self, notification_id):
+        return self.env['notification.notification.public'].search([('id', '=', notification_id)])
+
+    # remove a notification_id from notifications list read with notification_id condition
+    @api.model
+    def admin_delete_is_read_notification(self, notification_id):
+        if self.check_is_read_notification(notification_id):
+            self.env["is.read.notification"].search(
+                [('notification_id', '=', notification_id)]).unlink()
+        else:
+            return False
+
+    # remove multiple notification_id from notifications list read with notification_id list condition
+    @api.model
+    def admin_delete_list_unread_notification(self, notification_list):
+        for i in notification_list:
+            self.admin_delete_is_read_notification(i)
--- OdooOpenSourceModules/nbgsoftware_notification/models/notification_notification_public.py
+++ OdooOpenSourceModules/nbgsoftware_notification/models/notification_notification_public.py
@@ -7,6 +7,7 @@ from odoo.modules.module import get_resource_path
 
 class NotificationNotificationPublic(models.Model):
     _name = 'notification.notification.public'
+    _order = "id desc"
 
     def _get_users_domain(self):
         admin_group = self.env.ref('base.group_system')
@@ -14,12 +15,10 @@ class NotificationNotificationPublic(models.Model):
         return [('id', 'not in', admin_ids)]
 
     name = fields.Char(string='Title')
-    title = fields.Char(string='Title', compute='_compute_display_name_limited', store=True)
     content = fields.Html(string='Content', translate=True)
     create_notification_date_time = fields.Char(string="Created on", compute='_translate_time', store=True)
     tag_ids = fields.Many2many('notification.tag', 'tag_notification_rel', 'notification_id', 'tag_id', string="Tags")
     is_public = fields.Boolean(default=True)
-    is_notification_read = fields.Boolean(default=False)
     user_ids = fields.Many2many('res.users', string='Users notified', domain=_get_users_domain)
     is_notification_important = fields.Boolean(string='Mark to important', default=False)
     notification_important = fields.Binary(string="", compute='_compute_notification_important')
@@ -40,16 +39,6 @@ class NotificationNotificationPublic(models.Model):
             action_id.id, menu.id)
         return url
 
-    # get value count unread to show on notification badge for account of internal user and portal user
-    @api.model
-    def get_notification_count_unread(self):
-        notification_count_unread = len(self.list_of_notifications())
-        notification_count_read = len(self.list_of_read_notifications())
-        notification_count_unread = notification_count_unread - notification_count_read
-        if notification_count_unread < 0:
-            notification_count_unread = 0
-        return notification_count_unread
-
     # check hide/show notification badge between account of admin and internal user
     @api.model
     def is_show_notification_badge(self):
@@ -60,118 +49,6 @@ class NotificationNotificationPublic(models.Model):
     @api.model
     def get_user_id(self):
         return self.env.user.id
-
-    # list all notifications include notifications read & unread
-    def list_of_notifications(self):
-        self.init()
-        user = self.get_user_id()
-        self._cr.execute("""
-        SELECT * FROM public.notification_notification_public_res_users_rel WHERE res_users_id = %s
-        """ % user)
-        notifications_list = self._cr.dictfetchall()
-        return notifications_list
-
-    # list all notifications read
-    def list_of_read_notifications(self):
-        self.init()
-        user = self.get_user_id()
-        self._cr.execute("""SELECT * FROM public.is_read_notification WHERE user_id = %s""" % user)
-        notifications_list_read = self._cr.dictfetchall()
-        return notifications_list_read
-
-    @api.model
-    def is_read_notification_through_notification_id(self, notification_id):
-        self._cr.execute(
-            """
-            SELECT user_id, notification_id FROM public.is_read_notification
-            where user_id = %s and notification_id = %s
-            """ % (self.env.user.id, notification_id)
-        )
-        notification_list_read = self._cr.dictfetchall()
-        if notification_list_read:
-            return True
-        else:
-            return False
-
-    @api.model
-    def is_read_notification(self, notification_id, user_id):
-        self._cr.execute(
-            """
-            SELECT user_id, notification_id FROM public.is_read_notification
-            """
-        )
-        notification_list_read = self._cr.dictfetchall()
-        if {'user_id': user_id, 'notification_id': notification_id} in notification_list_read:
-            return True
-        else:
-            return False
-
-    def init(self):
-        self.env.cr.execute("""
-                       CREATE TABLE IF NOT EXISTS is_read_notification (
-                       id serial primary key,
-                        User_Id INT not null,
-                        Notification_Id INT not null,
-                        create_date timestamp without time zone DEFAULT (now() at time zone 'utc')
-                       );
-                   """)
-
-    # add notifications into notifications list read
-    @api.model
-    def update_list_read_notification(self, notification_list):
-        user_id = self.env.user.id
-        for i in notification_list:
-            self.update_is_read_notification(i, user_id)
-
-    # remove notifications from notifications list read
-    @api.model
-    def delete_list_unread_notification(self, notification_list):
-        user_id = self.env.user.id
-        for i in notification_list:
-            self.delete_is_read_notification(i, user_id)
-
-    @api.model
-    def update_is_read_notification_through_notification_id(self, notification_id):
-        self.env.cr.execute("""
-            insert into is_read_notification (Notification_Id, User_Id) 
-               select %s, %s
-               where not exists (
-                select null from is_read_notification 
-                where ( Notification_Id, User_Id) = (%s, %s)
-               )
-          """ % (notification_id, self.env.user.id, notification_id, self.env.user.id))
-
-    # add a notification into notifications list read
-    @api.model
-    def update_is_read_notification(self, notification_id, user_id):
-        self.env.cr.execute("""
-            insert into is_read_notification (Notification_Id, User_Id) 
-               select %s, %s
-               where not exists (
-                select null from is_read_notification 
-                where ( Notification_Id, User_Id) = (%s, %s)
-               )
-          """ % (notification_id, user_id, notification_id, user_id))
-
-    # remove a notification from notifications list read
-    @api.model
-    def delete_is_read_notification(self, notification_id, user_id):
-        self.env.cr.execute("""
-             delete from is_read_notification
-             where ( Notification_Id, User_Id) = (%s, %s) 
-        """ % (notification_id, user_id))
-
-    # limit char number display of title
-    @api.depends('name')
-    def _compute_display_name_limited(self):
-        for record in self:
-            if record.name:
-                if len(record.name) > 10:
-                    record.title = f"{record.name[:10]}..."
-                else:
-                    record.title = record.name
-            else:
-                record.title = False
 
     # show star icon use image
     def _compute_notification_important(self):
@@ -196,11 +73,3 @@ class NotificationNotificationPublic(models.Model):
             for i in d[-1]:
                 new_domain.append(['tag_ids', '=', i])
         return super(NotificationNotificationPublic, self).search_read(new_domain, fields, offset, limit, order)
-
-    @api.model
-    def get_notifications_data(self, notification_id):
-        self.env.cr.execute("""
-                  SELECT * FROM public.notification_notification_public
-                   WHERE id = %s
-                """ % notification_id)
-        return self._cr.dictfetchall()
